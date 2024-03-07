43bbb3148ffeee8a8027f7f564638bcf17a8d7b5
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 14:40:13 +0300

    Using for loop

---
+++ Dart with Flutter/main.dart
@@ -0,0 +1,12 @@
+//The for loop is used when we know how many times the block of code will execute
+void main()
+{
+  int num = 1;
+  // for loop to print 1-10 numbers
+  for(num; num<=3; num++)
+  {
+    // To print the number
+    print(num);
+  }
+}
+// The code prints numbers from 1 to 10 using a loop in Dart
