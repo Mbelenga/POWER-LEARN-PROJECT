2a00092f9ea9d8adfeee423ee127f66471a4e0dc
Mbelenga <mbelengaterence@gmail.com>
2024-04-11 00:50:16 +0300

    to be updated

---
+++ Web development/scripts/index.js
@@ -0,0 +1,22 @@
+// Using var
+var name = "Terrence"
+console.log(name);
+
+// Using let
+let job = "PLP Student"
+console.log(job);
+
+// Using const
+const PI = 3.14152; //number data type - floating number
+console.log(PI);
+
+var age = 29; //number data type - Integer number
+
+// boolean data type true or false
+var success = false;
+
+//Camelcase
+var firstName;
+
+//snake_case
+var last_name;
