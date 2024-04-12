efd55688af16c078926a8365a0ed17015d5cb05a
Mbelenga <mbelengaterence@gmail.com>
2024-04-12 17:58:29 +0300

    for ..in loop

---
+++ Dart with Flutter/8-main.dart
@@ -0,0 +1,9 @@
+//The code iterates over elements in the list list1 using a for-in loop and prints each element.
+void main()
+{
+  var list1 = [10,20,30,40,50];
+  for(var i in list1)
+  {
+    print(i);
+  }
+}
