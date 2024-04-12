1df0e59e17213c0e8c247bc4e7ea46261ea27370
Mbelenga <mbelengaterence@gmail.com>
2024-04-12 17:34:21 +0300

    if else if statements

---
+++ Dart with Flutter/5-main.dart
@@ -0,0 +1,10 @@
+void main() {
+  var age = 18;
+  if (age > 18) {
+    print ("Stacy is a voter in kenya");
+  } else if (age == 18) {
+    print ("Stacy just became eligible to vote in kenya");
+  } else {
+    print ("Stacy is still young to vote");
+  }
+}
