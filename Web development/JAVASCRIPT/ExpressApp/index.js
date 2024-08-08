54d93ef0fc4e53d85c6c0cfd9e65138ebf34fc6a
Mbelenga <mbelengaterence@gmail.com>
2024-08-08 03:08:58 +0300

    Express App

---
+++ Web development/JAVASCRIPT/ExpressApp/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+	res.send('Hello World');
+});
+app.listen(5000, function () {
+	console.log("Express App running at http://127.0.0.1:5000/");
+});
