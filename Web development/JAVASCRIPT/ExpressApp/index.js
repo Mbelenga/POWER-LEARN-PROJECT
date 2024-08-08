49e8e4e08e578b82c3b705d5123335de517b88aa
Mbelenga <mbelengaterence@gmail.com>
2024-08-09 02:06:47 +0300

    Prepares output in JSON format

--- Web development/JAVASCRIPT/ExpressApp/index.js
+++ Web development/JAVASCRIPT/ExpressApp/index.js
@@ -2,9 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname,"index.html"));
 })
-var server = app.listen(5000, function () {
+
+app.get('/process_get', function (req, res) {
+	// prepares output in JSON format
+	response = {
+		first_name:req.query.first_name,
+		last_name:req.query.last_name
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+})
+
+app.listen(5000, function () {
 	console.log("Express App running at http://127.0.0.1:5000/");
 })
