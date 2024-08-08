cc0e7226b641f4ec2906c121c476a677f51b1df2
Mbelenga <mbelengaterence@gmail.com>
2024-08-09 01:26:41 +0300

    Updated

--- Web development/JAVASCRIPT/ExpressApp/index.js
+++ Web development/JAVASCRIPT/ExpressApp/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 app.get('/', function (req, res) {
-	res.send('Hello World');
-});
-app.listen(5000, function () {
+	res.sendFile(path.join(__dirname,"index.html"));
+})
+var server = app.listen(5000, function () {
 	console.log("Express App running at http://127.0.0.1:5000/");
-});
+})
