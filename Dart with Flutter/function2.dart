fb85b7467b9923df66103f040a1bc08ec6688503
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 22:03:31 +0300

    function with No parameter and no return type

---
+++ Dart with Flutter/function2.dart
@@ -0,0 +1,8 @@
+// Function with No parameter and No return type
+void main() {
+  printName();
+}
+// nothing in parameter list and no return type
+void printName() {
+  print('Mary is a good student');
+}
