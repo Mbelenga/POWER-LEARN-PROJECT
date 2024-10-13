459255d0118abd50cf89fd9eccc9d30021b8bff6
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 22:51:42 +0300

    A function with no parameter and no return type

---
+++ Dart with Flutter/function4.dart
@@ -0,0 +1,11 @@
+// Function to return instructor's name
+String Instructorname() {
+  return "Allan";
+}
+
+void main() {
+  // calling the function and storing the result
+  String instructor = InstructorName();
+  // printing the instructor's name
+  print("The instructor's name is: $instructor");
+}
