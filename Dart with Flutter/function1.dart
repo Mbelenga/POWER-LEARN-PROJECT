36d176490d925550dce93771844a2c9172a5b360
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 21:58:45 +0300

    function that finds the sum of two numbers

---
+++ Dart with Flutter/function1.dart
@@ -0,0 +1,9 @@
+// Function to find sum of two numbers
+void findsum(int num1 int num2) {
+  int sum = num1 + num2;
+  print('The sum of $num1 and $num2 is: $sum');
+}
+void main() {
+  // calling the function with two numbers
+  findsum(10,20);
+}
