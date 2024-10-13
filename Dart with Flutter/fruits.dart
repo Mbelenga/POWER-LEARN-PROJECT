fa1e720d7a5244fea25a9f1b257616483d908e2c
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 23:26:34 +0300

    using forEach as an anonymous function to print each fruit

---
+++ Dart with Flutter/fruits.dart
@@ -0,0 +1,8 @@
+void main() {
+  // List of fruits
+  var fruits = ['Apple', 'Oranges', 'Bananas', 'Mangoes'];
+  // Using for each as an anonymous function to print each fruit
+  fruit.forEach((fruit) {
+    print(fruit);
+  });
+}
