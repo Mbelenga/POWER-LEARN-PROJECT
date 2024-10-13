85a6a9e53c1ca67eda5edee56e59cbdee078b380
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 23:06:32 +0300

    Function that adds two integers and returns the result

---
+++ Dart with Flutter/function5.dart
@@ -0,0 +1,11 @@
+// Function that adds two integers and returns the result
+int add(int a, int b) {
+  return a+b;
+}
+
+void main() {
+  // calling the function with two integers
+  int result = add(10, 20);
+  // printing the result
+  print("The sum of 10 and 20 is: $result");
+}
