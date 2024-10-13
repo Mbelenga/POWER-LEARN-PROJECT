e0b3944bb3ae264b8d271ae65d7f47a33d7d0001
Mbelenga <mbelengaterence@gmail.com>
2024-10-13 23:31:40 +0300

    printing a list of items

---
+++ Dart with Flutter/items.dart
@@ -0,0 +1,8 @@
+void main() {
+  // List of items
+  var items ['Laptop', 'Tablet', 'Monitor', 'Watch'];
+  // using an anonymous function for each to print each item
+  item.forEach((item) {
+    print(item);
+  })
+}
